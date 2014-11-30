db76e31222550e8c48b89fbae1dcc215fe6c1765
Sinthu <s9556748@mail.zih.tu-dresden.de>
2014-11-30 14:50:24 +0100

    navbarkeit geändert

--- foobar/src/main/java/jrmds/controller/UserEditController.java
+++ foobar/src/main/java/jrmds/controller/UserEditController.java
@@ -3,10 +3,10 @@ package jrmds.controller;
 import jrmds.user.UserManagement;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class UserEditController {
 	@Autowired
 	private UserManagement usr;
