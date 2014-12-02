27b3b85c24010882bac0fec70fc603715370562a
Sinthu <s9556748@mail.zih.tu-dresden.de>
2014-12-02 19:29:13 +0100

    scheiß merge....

--- README.md
+++ README.md
@@ -14,7 +14,7 @@ Installationsanleitung:
 und folgendermaßen anpassen (also auskommentieren und binding auf localhost):
   org.neo4j.server.webserver.address=[::1]
 
-3) dieses Repo Clonen und als Maven Projekt importieren
+3) dieses Repo Clonen und in der aktuellen Spring-Tool-Suite (oder Eclipse mit STS Plugin) als Maven Projekt importieren
 4) unter test/jrmdsManagementTest ausführen, dies erstellt in der Datenbank als "Nebenprodukt" zwei Projekte mit vielerlei Gruppen und Regeln und Beziehungen
 5) wenn die Application.java ausgeführt wird, kann unter localhost:8080 eine einfache klickbare Seite aufgerufen werden
 ```
--- foobar/src/main/java/jrmds/main/RuleRepository.java
+++ foobar/src/main/java/jrmds/main/RuleRepository.java
@@ -13,7 +13,9 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RuleRepository extends CrudRepository<Component, Long> {
-	@Query("match (n:Project {name:{0}})--(m:Component {refID:{1} , type:{2}}) return m;")
+	@Query("match (p:Project)--(n:Component) WHERE p.name={0} RETURN n;")
+	Set<Component> findAnyComponentOfProject(String projectName);
+	@Query("match (n:Project {name:{0}})--(m:Component {refID:{1}, type:{2}}) return m;")
 	Component findByRefIDAndType(String projectName, String refID, ComponentType type);
 	@Query("match (n:Component)-[r:DEPENDSON]->(m:Component {refID:{1}})--(p:Project {name:{0}}) return n;")
 	Set<Component> findUpstreamRefs(String projectName, String refID);
@@ -21,6 +23,8 @@ public interface RuleRepository extends CrudRepository<Component, Long> {
 	Set<Component> findDownstreamRefs(String projectName, String refID);
 	@Query("match (p:Project)--(n) where id(n)={0} return p")
 	Project findProjectContaining(Long id);
-	@Query("match (n:Component {refID:{0}})-[r:DEPENDSON*1..]-(m:Component {refID:{1}) return n,m")
-	Set<Component> findAnyConnectionBetween(Component first, Component second);
+	@Query("match (p:Project {name:{0}})--(n:Component {refID:{1}})-[r:DEPENDSON*1..]-(m:Component {refID:{2}}) return n limit 1")
+	Component findAnyConnectionBetween(String projectName, String firstRefID, String secondRefID);
+	@Query("match q=(p:Project)--(n:Component)-[r:DEPENDSON*1..]->(m:Component) where n.refID={1} AND p.name={0} return filter(n in nodes(q) where n:Component);")
+	Set<Component> findAllReferencedNodes(String projectName, String refID);
 }
