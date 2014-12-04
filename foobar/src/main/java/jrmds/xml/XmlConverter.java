2b5d1788a061e651de8e51472f520bb47f6066a5
HandBreit <brit@britundlars.de>
2014-12-04 19:18:51 +0100

    controllerundconverteraktualsiert

--- foobar/src/main/java/jrmds/xml/XmlConverter.java
+++ foobar/src/main/java/jrmds/xml/XmlConverter.java
@@ -2,64 +2,113 @@ package jrmds.xml;
 
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.util.List;
+import java.io.InvalidObjectException;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
-import javax.xml.bind.Unmarshaller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import jrmds.model.Component;
+import jrmds.model.Group;
 
-import com.fasterxml.jackson.core.JsonParser.Feature;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
-import jrmds.main.*;
-import jrmds.model.Rule;
-
-@RestController
 public class XmlConverter {
 	
-	@RequestMapping ("/welcome")
-	String welcome(
-			@RequestParam(value = "name", defaultValue = "World") String name) throws JAXBException{
-
-		try {
-			JAXBContext jCtx = JAXBContext.newInstance(Rule.class);
-			Unmarshaller fromXml = jCtx.createUnmarshaller();
-			Rule rXml = (Rule)fromXml.unmarshal( new FileReader("D:\\model.xml"));
-
-			ObjectMapper mapper = new ObjectMapper();
-	        System.out.println(mapper.writeValueAsString(rXml));
-			
-
-
-			Rule fromJson = mapper.readValue(new FileReader("D:\\model.json"), Rule.class);
-			
-			Marshaller toXmlMarshaller = jCtx.createMarshaller();
-			toXmlMarshaller.marshal(fromJson, System.out);
-			
-			
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-
-		return "Welcome ".concat(name).concat("!");		
-	}
+	
 	public void xmlToObjects(){
 		
 	}
 	
-	public void objectsToXml(){
-		
+	public static void objectsToXml(Set<jrmds.model.Component> setComp) throws InvalidObjectException, JAXBException{
+			XmlRule rule = new XmlRule();
+			rule.setConcepts(new HashSet<XmlConcept>());
+			rule.setConstraints(new HashSet<XmlConstraint>());
+			rule.setGroups(new HashSet<XmlGroup>());
+			rule.setTemplates(new HashSet<XmlTemplate>());
+
+			for (jrmds.model.Component comp : setComp)
+			{
+				switch (comp.getType())
+				{
+				case CONCEPT:
+					rule.getConcepts().add(ConvertConcept(comp));
+					break;
+				case CONSTRAINT:
+					rule.getConstraints().add(ConvertConstraint(comp));
+					break;
+				case GROUP:
+					//add group
+					rule.getGroups().add(ConvertGroup(comp));
+					break;
+				case TEMPLATE:
+					//add template
+					break;
+
+				default:
+					throw new InvalidObjectException("Typ nicht unterstützt");
+				}
+			}
+			
+			JAXBContext jCtx = JAXBContext.newInstance(jrmds.model.Rule.class);
+			Marshaller toXmlMarshaller = jCtx.createMarshaller();
+			toXmlMarshaller.marshal(rule, new File("D:\\Rule.xml"));
+			
 	}
+
+private static XmlGroup ConvertGroup(jrmds.model.Component group) throws InvalidObjectException
+{
+	XmlGroup xg = new XmlGroup();
+	xg.setConcepts(new HashSet<XmlConcept>());
+	xg.setConstraints(new HashSet<XmlConstraint>());
+	xg.setGroups(new HashSet<XmlGroup>());
+
+	set2group(xg, ((Group)group).getReferencedComponents());
+	return xg;
+}
+
+private static XmlConcept ConvertConcept(jrmds.model.Component comp)
+{
+	XmlConcept c = new XmlConcept();
+	c.setCypher(comp.getCypher());
+	c.setId(comp.getRefID());	// TODO: getId()? Oder fehlt sogar was?
+	c.setDescription(comp.getDescription());
+	return c;
+}
+
+private static XmlConstraint ConvertConstraint(Component comp) {
+	XmlConstraint c = new XmlConstraint();
+	c.setCypher(comp.getCypher());
+	c.setId(comp.getRefID());	// TODO: getId()? Oder fehlt sogar was?
+	c.setDescription(comp.getDescription());
+	return c;
+}
+
+private static void set2group(XmlGroup group, Set<jrmds.model.Component> setComp) throws InvalidObjectException
+{
+	for (jrmds.model.Component comp : setComp)
+	{
+		switch (comp.getType())
+		{
+		case CONCEPT:
+			group.getConcepts().add(ConvertConcept(comp));
+			break;
+		case CONSTRAINT:
+			//add constraint
+			group.getConstraints().add(ConvertConstraint(comp));
+			break;
+		case GROUP:
+			//add group
+			group.getGroups().add(ConvertGroup(comp));
+			break;
+		
+		default:
+			throw new InvalidObjectException("Typ nicht unterstützt");
+		}
+	}
+}
+
 	
 	public void objectsToJson(){
 		
