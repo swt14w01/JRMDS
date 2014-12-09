6af83a827bc31e3a1799ea4109ea2e6bce7606a5
Sinthu <s9556748@mail.zih.tu-dresden.de>
2014-12-09 21:33:53 +0100

    gettin pro

--- foobar/src/main/resources/static/js/popup.js
+++ foobar/src/main/resources/static/js/popup.js
@@ -5,7 +5,7 @@ jQuery(function($) {
 		if (popUpState == false) {
 			$("#add_rule_menu").fadeIn("fast");
 			$(this).css('background-image','none');
-			$(this).css('color','none');
+			$(this).css('color','#0086e4');
 			popUpState = true;
 		}
 		return false;
@@ -15,6 +15,7 @@ jQuery(function($) {
 		if (popUpState == true) {
 			$("#add_rule_menu").fadeOut("fast");
 			$("#addRule").css('background-image','url(../img/options_button_bg.png)');
+			$("#addRule").css('color','black');
 			popUpState = false;
 		}
 
