49275b06ee013cffeb7267fd488c0c6c29123f19
Sinthu <s9556748@mail.zih.tu-dresden.de>
2014-12-12 00:18:37 +0100

    added some sexy jQuery

--- foobar/src/main/resources/static/js/validateSearch.js
+++ foobar/src/main/resources/static/js/validateSearch.js
@@ -1,13 +1,29 @@
-alert("test");
-var submitButt = $("#search_button");
+
+alert("sd");
+var submitButt = $(".searchButton");
 var searchInput = $("#search_input");
+var searchJRMDS ="Search JRMDS";
+// INIT BUTTON
+submitButt.attr("disabled", !(searchInput.val()!=searchJRMDS && searchInput.val()!=""));
 
 
+// WHILE TYPING, CHANGE BUTTON
+searchInput.keyup(function() {
+	submitButt.attr("disabled", !(searchInput.val()!=searchJRMDS && searchInput.val()!=""));
 
-submitButt.attr("disabled", !((searchInput.val()!="Search JRMDS")) && (searchInput.val().length>0)));
+});
 
+// WHEN CLICKING ON SEARCHFIELD, SET VALUE TO ""
+searchInput.focus(function() {
+	if (searchInput.val() == searchJRMDS) {
+		searchInput.val("");
+	}
+
+});
+
+// WHEN CLICKING OUTSIDE SEARCHFIELD, RESET VALUE TO "Search JRMDS"
 searchInput.blur(function() {
-
-	submitButt.attr("disabled", !((searchInput.val()!="Search JRMDS")) && (searchInput.val().length>0)));
-
+	if (searchInput.val().length == 0) {
+		searchInput.val(searchJRMDS);
+	}
 });
