c34aabb7881d5dfeb45dcec8b7edfbef8d669afd
zarhack <leroy.buchholz@gmx.de>
2014-12-15 17:51:40 +0100

    Signin/Signout-button

--- foobar/src/main/java/jrmds/controller/ViewController.java
+++ foobar/src/main/java/jrmds/controller/ViewController.java
@@ -4,6 +4,7 @@ import jrmds.model.RegistredUser;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import jrmds.security.CurrentUser;
@@ -11,6 +12,11 @@ import jrmds.security.CurrentUser;
 @Controller
 public class ViewController extends WebMvcConfigurerAdapter{
 
+	@RequestMapping(value = "/signin", method=RequestMethod.POST)
+	public String loginPage() {
+		return "login";
+	}
+	
 	@RequestMapping(value = "/test")
 	public String currentUserTest(@CurrentUser RegistredUser user) {
 	    if(user != null) {
