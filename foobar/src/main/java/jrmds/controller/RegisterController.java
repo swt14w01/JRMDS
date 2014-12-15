5bdd2ad447f6663bf53c076a0148586c422b2dcf
zarhack <leroy.buchholz@gmx.de>
2014-12-15 09:52:17 +0100

    ne Leerzeile gelöscht ;)

--- foobar/src/main/java/jrmds/controller/RegisterController.java
+++ foobar/src/main/java/jrmds/controller/RegisterController.java
@@ -8,7 +8,6 @@ import jrmds.user.UserManagement;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
