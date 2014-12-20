1567f327600932d555481171f764d080e5ec07f8
Stephanie <Steph.Schoene@gmx.de>
2014-12-20 22:39:14 +0100

    Added NullExc. Tests to RuleTest

--- foobar/src/test/java/test/RuleTest.java
+++ foobar/src/test/java/test/RuleTest.java
@@ -18,38 +18,50 @@ public class RuleTest {
 		//Constraint Check
 		assertEquals("The RefID is wrong!", "ruleconstraint1", ruleconst1.getRefID());
 		assertEquals("The Type is wrong!", ComponentType.CONSTRAINT, ruleconst1.getType());
-		assertEquals("Severity has to be null!", null, ruleconst1.getSeverity());
+		assertEquals("Severity has to be info!", "info", ruleconst1.getSeverity());
 		
 		//Concept Check
 		assertEquals("The RefID is wrong!", "ruleconcept1", ruleconc1.getRefID());
 		assertEquals("The Type is wrong!", ComponentType.CONCEPT, ruleconc1.getType());
-		assertEquals("Severity has to be null!", null, ruleconc1.getSeverity());
+		assertEquals("Severity has to be info!", "info", ruleconc1.getSeverity());
 	}
 	
-	
+	@Test
 	public void setSeverityTest(){
+		ruleconc1.setSeverity("blocker");
+		assertEquals("Severity has to be blocker!", "blocker", ruleconc1.getSeverity());
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void setFalseSeverityTest(){
 		//get-Test not possible
 		ruleconc1.setSeverity("sev1");
-		ruleconst1.setSeverity("sev2");
-		assertEquals("Method setSeverity() is not correct!", "sev1", ruleconc1.getSeverity());
-		assertEquals("Method setSeverity() is not correct!", "sev2", ruleconst1.getSeverity());
+		ruleconc1.getSeverity();
+	}
+	
+	@Test(expected = NullPointerException.class)
+	public void setSeverityNulLTest(){
+		ruleconc1.setSeverity(null);
 	}
 	
 	@Test
 	public void CheckSecondConstructor(){
-		ruleconst1.setSeverity("sev1");
-		ruleconc1.setSeverity("sev2");
+		ruleconst1.setSeverity("info");
+		ruleconc1.setId(1L);
+		ruleconc1.setSeverity("blocker");
+		ruleconst1.setId(2L);
+		
 		Rule ruleconst2 = new Constraint(ruleconst1);
 		Rule ruleconc2 = new Concept(ruleconc1);
 		
 		//Constraint Check
 		assertEquals("The RefID is wrong!", "ruleconstraint1", ruleconst2.getRefID());
 		assertEquals("The Type is wrong!", ComponentType.CONSTRAINT, ruleconst2.getType());
-		assertEquals("The Severity is wrong!","sev1", ruleconst2.getSeverity());
+		assertEquals("The Severity is wrong!","info", ruleconst2.getSeverity());
 		
 		//Concept Check
 		assertEquals("The RefID is wrong!", "ruleconcept1", ruleconc2.getRefID());
 		assertEquals("The Type is wrong!", ComponentType.CONCEPT, ruleconc2.getType());
-		assertEquals("The Severity is wrong!","sev2", ruleconc2.getSeverity());
+		assertEquals("The Severity is wrong!","blocker", ruleconc2.getSeverity());
 	}
 }
