a5549a5d822e9251154682618504d6cc99165443
zarhack <leroy.buchholz@gmx.de>
2015-01-10 12:04:03 +0100

    header.css

--- foobar/src/main/java/jrmds/validation/annotations/EmailAdressValidity.java
+++ foobar/src/main/java/jrmds/validation/annotations/EmailAdressValidity.java
@@ -10,6 +10,11 @@ import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
 import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
+/**
+ * 
+ * @author Leroy Buchholz
+ *
+ */
 @Target({TYPE, FIELD, ANNOTATION_TYPE}) 
 @Retention(RUNTIME)
 @Constraint(validatedBy = EmailAdressValidation.class)
