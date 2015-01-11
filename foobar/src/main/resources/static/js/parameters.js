802f9509d17bfe9d19951396bcb0c1e109e7c8a0
Sinthu <s9556748@mail.zih.tu-dresden.de>
2015-01-11 19:24:31 +0100

    hm...

--- foobar/src/main/resources/static/js/parameters.js
+++ foobar/src/main/resources/static/js/parameters.js
@@ -2,13 +2,15 @@ var count =$("#paramDiv span").size();
 var newParamButton = $("#newParamButton");
 var paramDiv = $("#paramDiv");
 
+alert(count);
 
 newParamButton.click(function() {
 	
 
-	var newParam = "<span><input type='text' class='parameter_input' th:name='toUpdateName'/> <input type='text' class='parameter_input' th:name='toUpdateValue'/> <input type='checkbox' th:name='isString'/> <input type='hidden' th:name='toUpdateId' th:value='" + count + "'/></span>";
+	var newParam = "<span><input type='text' class='parameter_input' name='toUpdateName'/> <input type='text' class='parameter_input' name='toUpdateValue'/> <input type='checkbox' name='isString'/> <input type='hidden' name='toUpdateId' value='" + count + "'/></span>";
 	paramDiv.append(newParam);
 	count = $("#paramDiv span").size();
+	alert(count);
 
 	
 });
