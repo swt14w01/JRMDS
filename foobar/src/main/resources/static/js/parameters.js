9869392a584076fbffe02b8f6a937069bbd4c41d
Sinthu <s9556748@mail.zih.tu-dresden.de>
2015-01-11 20:21:32 +0100

    bla

--- foobar/src/main/resources/static/js/parameters.js
+++ foobar/src/main/resources/static/js/parameters.js
@@ -3,7 +3,7 @@ var newParamButton = $("#newParamButton");
 var paramDiv = $("#paramDiv");
 
 
-$('.cancelButton').click(function() {
+$('.removeButton').click(function() {
 	
 	$(this).parents('span').fadeOut(300, function() {$(this).remove();});
 	//$(this).parents('span').fadeOut();
@@ -15,16 +15,17 @@ newParamButton.click(function() {
 
 
 	var newParam = "<span>" +
+						"<input type='button' class='removeButton' value='-' />"+
 						"<input type='text' class='parameter_input' name='toUpdateName'/> " +
 						"<input type='text' class='parameter_input' name='toUpdateValue'/> " +
-						"<input type='checkbox' name='isString'/> " +
+						"<input type='checkbox' name='isString' value='" + count + "'/> " +
 						"<input type='hidden' name='toUpdateId' value='" + count + "'/>" +
-						"<input type='button' class='cancelButton' value='-' />"+
+						
 					"</span>";
 	paramDiv.append(newParam);
 	count = $("#paramDiv span").size();
 
-	$('.cancelButton').click(function() {
+	$('.removeButton').click(function() {
 		
 		$(this).parents('span').fadeOut(300, function() {$(this).remove();});
 		// $(this).parents('span').remove();
