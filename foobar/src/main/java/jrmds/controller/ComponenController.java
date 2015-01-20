7c6d3da2a1a2d77c867416388f4f804cd5dbd87f
PoisonNuke <mail@poisonnuke.de>
2015-01-20 17:04:47 +0100

    deleted unnecceary import

--- foobar/src/main/java/jrmds/controller/ComponenController.java
+++ foobar/src/main/java/jrmds/controller/ComponenController.java
@@ -28,7 +28,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.thymeleaf.Template;
 
 @Controller
 public class ComponenController {
