7f1d62173b0dd41ed915e4d337a44005d69e6f53
PoisonNuke <mail@poisonnuke.de>
2015-01-21 18:09:25 +0100

    bugg

--- foobar/src/main/java/jrmds/controller/ProjectController.java
+++ foobar/src/main/java/jrmds/controller/ProjectController.java
@@ -711,15 +711,16 @@ public class ProjectController {
 	      	 */
 	      	for(Component c : toAdd){
 	      		
-	      		Component compare = null;
+	      		Component compare;
 	      		switch(c.getType()){
-	      		case GROUP: compare = jrmds.getComponent(targetProject, new Group(c.getRefID()));break;
-	      		case CONSTRAINT: compare = jrmds.getComponent(targetProject, new Constraint(c.getRefID()));break;
-	      		case CONCEPT: compare = jrmds.getComponent(targetProject, new Concept(c.getRefID()));break;
-	      		case TEMPLATE:compare = jrmds.getComponent(targetProject, new QueryTemplate(c.getRefID()));break;
+	      		case GROUP: compare = new Group(c.getRefID()); break;
+	      		case CONSTRAINT: compare = new Constraint(c.getRefID()); break;
+	      		case CONCEPT: compare = new Concept(c.getRefID()); break;
+	      		case TEMPLATE:compare = new QueryTemplate(c.getRefID()); break;
 	      		default: throw new IllegalArgumentException("Component-type not specified");
 	      		}
 	      		
+	      		compare = jrmds.getComponent(targetProject,compare);
 	      		if(compare!=null) {System.out.println("FAILURE INCOMING!");jrmds.deleteComponent(targetProject, compare);}
 	      		jrmds.saveComponent(targetProject, c);
 	      		 
