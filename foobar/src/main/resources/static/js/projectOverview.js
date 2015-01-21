2801608c6ac10f2e8a7ea239251de5bd7182604e
Sinthu <s9556748@mail.zih.tu-dresden.de>
2015-01-21 01:10:07 +0100

    UI stuff

--- foobar/src/main/resources/static/js/projectOverview.js
+++ foobar/src/main/resources/static/js/projectOverview.js
@@ -1,18 +1,55 @@
 jQuery(document).ready(function() {
-	$("#menu_group").click(function() {
-		$("#tab1").toggle("fast");
-		$(this).toggleClass("active");
-	});
-	$("#menu_concept").click(function() {
-		$("#tab2").toggle("fast");
-		$(this).toggleClass("active");
-	});
-	$("#menu_contraint").click(function() {
-		$("#tab3").toggle("fast");
-		$(this).toggleClass("active");
-	});
-	$("#menu_template").click(function() {
-		$("#tab4").toggle("fast");
-		$(this).toggleClass("active");
-	});
+	
+	$("#tab1").show();
+	if($("#nGroups").html()=="0") {
+		$("#menu_group").toggleClass("inactive");
+	}
+	else {
+		$("#menu_group").toggleClass("active");
+		
+		$("#menu_group").click(function() {
+			$("#tab1").toggle("fast");
+			$(this).toggleClass("active");
+		});
+		
+	}
+	
+	$("#tab2").show();
+	if($("#nConcepts").html()=="0") {
+		$("#menu_concept").toggleClass("inactive");
+	}
+	else {
+		$("#menu_concept").toggleClass("active");
+		$("#menu_concept").click(function() {
+			$("#tab2").toggle("fast");
+			$(this).toggleClass("active");
+		});
+	}
+	
+	
+	$("#tab3").show();
+	if($("#nConstraints").html()=="0") {
+		$("#menu_contraint").toggleClass("inactive");
+	}
+	else {
+		$("#menu_contraint").toggleClass("active");
+		$("#menu_contraint").click(function() {
+			$("#tab3").toggle("fast");
+			$(this).toggleClass("active");
+		});
+	}
+	
+	$("#tab4").show();
+	if($("#nTemplates").html()=="0") {
+		$("#menu_template").toggleClass("inactive");
+	}
+	else {
+		$("#menu_template").toggleClass("active");
+		$("#menu_template").click(function() {
+			$("#tab4").toggle("fast");
+			$(this).toggleClass("active");
+		});
+	}
+		
+
 });
