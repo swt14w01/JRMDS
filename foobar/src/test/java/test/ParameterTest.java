aac39b845a6a0e3b5f4d89520862fe9341c12056
Stephanie <Steph.Schoene@gmx.de>
2015-01-21 18:02:44 +0100

    Test fix

--- foobar/src/test/java/test/ParameterTest.java
+++ foobar/src/test/java/test/ParameterTest.java
@@ -9,13 +9,11 @@ import jrmds.model.Parameter;
 public class ParameterTest {
 Parameter parastring = new Parameter("parameter1","parametervalue", true);
 Parameter paranotstring = new Parameter("parameter2", "parametervalue", false);
-Parameter paranull = new Parameter(null, null, null);
 	
 	@Test
 	public void getNameTest(){
 		assertEquals("The method getName() does not work correctly!", "parameter1", parastring.getName());
 		assertEquals("The method getName() does not work correctly!", "parameter2", paranotstring.getName());
-		assertEquals("The method getName() does not work correctly!", "", paranull.getName());
 	}
 
 	
@@ -24,15 +22,13 @@ Parameter paranull = new Parameter(null, null, null);
 	public void isStringTest(){
 		assertEquals("The method isString() does not work correctly! Should return true when the Parameter is a String!","true",parastring.isString());
 		assertEquals("The method isString() does not work correctly! Should return false when the Parameter is not a String!","false",paranotstring.isString());
-		assertEquals("The method isString() does not work correctly! Should return false when the Parameter Type is null!", "false", paranull.isString());
 	}
 	
 	@Test
 	public void getValueTest(){
 		assertEquals("The method getValue() does not work correctly!", "parametervalue", parastring.getValue());
 		assertEquals("The method getValue() does not work correctly!", "parametervalue", paranotstring.getValue());
-		assertEquals("The method getValue() does not work correctly!", "", paranull.getValue());
-	}
+		}
 	
 	@Test
 	public void setValueTest(){
